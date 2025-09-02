07c2303d9f7b55db23426797675f1d18c715f236
Abinayaasree-KS <95009340+Abinayaasree-KS@users.noreply.github.com>
2025-09-02 19:16:34 +0530

    Time: 29 ms (40.00%) | Memory: 27.6 MB (15.46%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
        int n = s1.size();
-        int m = s2.size();
-
-        // Initialize two vectors to store the current and previous rows of the DP table
-        vector<int> prev(m + 1, 0), cur(m + 1, 0);
-
-        // Base case is covered as we have initialized the prev and cur vectors to 0.
-
-        for (int ind1 = 1; ind1 <= n; ind1++) {
-            for (int ind2 = 1; ind2 <= m; ind2++) {
-                if (s1[ind1 - 1] == s2[ind2 - 1])
-                    cur[ind2] = 1 + prev[ind2 - 1]; // Characters match, increment LCS length
-                else
-                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]); // Characters don't match, consider the maximum from above or left
-            }
-            prev = cur; // Update the previous row with the current row
+    int m = s2.size();
+    int cnt = 0;
+    vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+    
+    if(n==0){
+        // cout<<m;
+        return m;
+    }
+    else if(m==0){
+        // cout<<n;
+        return n;
+    }
+    
+    for(int i=1; i<=n; i++){
+        for(int j =1; j<=m; j++){
+        if(s1[i-1] == s2[j-1]){
+            dp[i][j] = 1 + dp[i-1][j-1];
         }
-
-        return prev[m]; 
+        else{
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+        }
+        
+    }
+    
+    return dp[n][m];
     }
 };
