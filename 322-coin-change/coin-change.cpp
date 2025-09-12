6fb400e15ff72b5691f069e250d0fdd0c42ad065
Abinayaasree-KS <95009340+Abinayaasree-KS@users.noreply.github.com>
2025-09-12 21:54:51 +0530

    Time: 15 ms (93.22%) | Memory: 18 MB (74.46%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,35 @@
+// class Solution {
+// public:
+//     int coinChange(vector<int>& coins, int amount) {
+//       if(amount < 1) return 0;
+//       vector<int> minCoinDP(amount + 1, INT_MAX);
+//         minCoinDP[0] = 0; // base case
+
+//         for (int i = 1; i <= amount; i++) {
+//             for (int coin : coins) {
+//                 if (coin <= i && minCoinDP[i - coin] != INT_MAX) {
+//                     minCoinDP[i] = min(minCoinDP[i], 1 + minCoinDP[i - coin]);
+//                 }
+//             }
+//         }
+
+//         return minCoinDP[amount] == INT_MAX ? -1 : minCoinDP[amount];  
+//     }
+// };
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
       if(amount < 1) return 0;
-      vector<int> minCoinDP(amount + 1, INT_MAX);
-        minCoinDP[0] = 0; // base case
-
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i && minCoinDP[i - coin] != INT_MAX) {
-                    minCoinDP[i] = min(minCoinDP[i], 1 + minCoinDP[i - coin]);
-                }
+      vector<int> dp(amount + 1, INT_MAX);
+      dp[0] = 0;
+      for(int i =1; i<=amount; i++){
+        for(int coin : coins){
+            if(coin <= i && dp[i-coin] != INT_MAX){
+                dp[i] = min(dp[i], 1+dp[i-coin]);
             }
         }
-
-        return minCoinDP[amount] == INT_MAX ? -1 : minCoinDP[amount];  
+      } 
+      return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
